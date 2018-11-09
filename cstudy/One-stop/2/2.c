c82169d66a96e0ccfff691ed4761079e7ceba44b
Loner1024 <xth12138@gmail.com>
2018-11-10 00:59:27 +0800

    第二章

--- cstudy/One-stop/2/2.c
+++ cstudy/One-stop/2/2.c
@@ -2,7 +2,12 @@
 
 int main(void)
 {
+    char firstletter; //定义一个字符型变量
+    int hour,minute; // 定义两个整数变量
+    firstletter = 'a'; //给变量赋值
+    hour = 11;
+    minute = 59;
     /* code */
-    printf("hello\t");
+    printf("Current time is %d:%d\n",hour,minute);
     return 0;
 }
